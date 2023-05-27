2be551cd09241d07d448f782c5104fa10f7fcb3b
Phil Wang <lucidrains@gmail.com>
2023-05-27 08:44:21 -0700

    attention, but of course

--- flash_genomics_model/flash_genomics_model.py
+++ flash_genomics_model/flash_genomics_model.py
@@ -8,7 +8,37 @@ from flash_genomics_model.attend import Attend
 
 # functions
 
-# helper classes
+# attention
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8,
+        flash = True
+    ):
+        super().__init__()
+        self.heads = heads
+        dim_inner = heads * dim_head
+        self.attend = Attend(flash = flash)
+
+        self.to_qkv = nn.Linear(dim, dim_inner * 3, bias = False)
+        self.to_out = nn.Linear(dim_inner, dim, bias = False)
+
+    def forward(
+        self,
+        x,
+        mask = None
+    ):
+        h = self.heads
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
+
+        out = self.attend(q, k, v, mask = mask)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
 
 # main class
 
